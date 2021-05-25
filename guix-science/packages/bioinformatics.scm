25f829412ceb283f35ca1db351a24044ee29c1a1
Roel Janssen <roel@gnu.org>
2021-05-25 15:36:17 +0200

    bioinformatics: Add caveman.
    * guix-science/packages/bioinformatics.scm (caveman): New variable.

--- guix-science/packages/bioinformatics.scm
+++ guix-science/packages/bioinformatics.scm
@@ -1389,3 +1389,45 @@ Pan-Cancer Analysis Project")
 generating consistent Vcf headers.  It primarily exists to prevent code
 duplication between some other projects.")
    (license license:agpl3+)))
+
+(define-public caveman
+  (package
+   (name "caveman")
+   (version "1.9.4")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "https://github.com/cancerit/CaVEMan/archive/"
+                                version ".tar.gz"))
+            (sha256
+             (base32 "19h631avgknidbchk6997ckgd60072nlbg9hv2zxn6vvq3fpsyb8"))))
+   (build-system gnu-build-system)
+   (arguments
+    `(#:make-flags (list (string-append
+                          "HTSLOC=" (assoc-ref %build-inputs "htslib")))
+      #:tests? #f ; Tests require a network connection.
+      #:phases
+      (modify-phases %standard-phases
+        (delete 'configure)
+        (add-before 'build 'patch-out-tests
+          (lambda _
+            (substitute* "Makefile"
+             (("copyscript test") "copyscript"))))
+        (replace 'install
+          (lambda* (#:key outputs #:allow-other-keys)
+            (let ((bin (string-append (assoc-ref outputs "out") "/bin")))
+              (install-file "bin/caveman" bin)
+              (install-file "bin/generateCavemanUMNormVCF" bin)
+              (install-file "bin/mergeCavemanResults" bin)))))))
+   (inputs
+    `(("htslib" ,htslib)
+      ("zlib" ,zlib)
+      ("perl" ,perl)))
+   (home-page "http://cancerit.github.io/CaVEMan/")
+   (synopsis "Implementation of an SNV expectation maximisation algorithm for
+calling single base substitutions in paired data")
+   (description "A C implementation of the CaVEMan program.  Uses an expectation
+maximisation approach to calling single base substitutions in paired data.  It
+is designed for use with a compute cluster.  Most steps in the program make use
+of an index parameter.  The split step is designed to divide the genome into
+chunks of adjustable size to optimise for runtime/memory usage requirements.")
+   (license license:agpl3+)))
