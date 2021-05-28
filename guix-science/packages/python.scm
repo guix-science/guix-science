94875c12a5c9cff96c2395574e05999c8568d53f
Roel Janssen <roel@gnu.org>
2021-05-28 16:38:41 +0200

    python: Add Nanopore-related packages.
    * guix-science/packages/python.scm: Add multiple packages.

--- guix-science/packages/python.scm
+++ guix-science/packages/python.scm
@@ -411,3 +411,67 @@ telomere length for a paired end, whole genome sequencing sample.")
 from WGS Data. It is designed to take BAM files from a tumor and/or a control
 sample as input. The tool was developed at the German Cancer Research Center.")
     (license license:gpl3+)))
+
+(define-public python-nanomath
+  (package
+    (name "python-nanomath")
+    (version "0.23.1")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "nanomath" version))
+              (sha256
+               (base32
+                "04b0n1qqyqq0id55zxp2dl3zj367gf59c8jilca406aqnjryv9sl"))))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-numpy" ,python-numpy)
+       ("python-pandas" ,python-pandas)))
+    (home-page "https://github.com/wdecoster/nanomath")
+    (synopsis "Simple math function for other Oxford Nanopore scripts")
+    (description "This package contains a few simple math function for other
+Oxford Nanopore processing scripts.")
+    (license license:expat)))
+
+(define-public python-nanoget
+  (package
+    (name "python-nanoget")
+    (version "1.8.0")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "nanoget" version))
+              (sha256
+               (base32
+                "0cs5sc2i7mfbikgssfaia28bagvka2a8qpmdzbf6i27piv2c7kyz"))))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-biopython" ,python-biopython)
+       ("python-nanomath" ,python-nanomath)
+       ("python-numpy" ,python-numpy)
+       ("python-pandas" ,python-pandas)
+       ("python-pysam" ,python-pysam)))
+    (home-page "https://github.com/wdecoster/nanoget")
+    (synopsis "Functions for Oxford Nanopore sequencing data and alignments")
+    (description "This package contains functions to extract information from
+Oxford Nanopore sequencing data and alignments.")
+    (license license:expat)))
+
+(define-public python-nanostat
+  (package
+    (name "python-nanostat")
+    (version "1.1.2")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "NanoStat" version))
+              (sha256
+               (base32
+                "1mr81xl08qw1vyl552snnxafzmbg9rv9lskyzvzqg8dhm8baslya"))))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-nanoget" ,python-nanoget)
+       ("python-nanomath" ,python-nanomath)))
+    (home-page "https://github.com/wdecoster/nanostat")
+    (synopsis "Statistics for Oxford Nanopore sequencing data and alignments")
+    (description
+     "This package provides procedures to calculate statistics for Oxford
+Nanopore sequencing data and alignments.")
+    (license license:expat)))
