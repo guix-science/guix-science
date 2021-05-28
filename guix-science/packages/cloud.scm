9c4eb659536b52cfe1e621359dea7fefd8ddf201
Roel Janssen <roel@gnu.org>
2021-05-28 14:56:52 +0200

    cloud: Add more Google Cloud-related packages.
    * guix-science/packages/cloud.scm: Add multiple packages.

--- guix-science/packages/cloud.scm
+++ guix-science/packages/cloud.scm
@@ -19,7 +19,9 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages base)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system python)
   #:use-module (guix download)
   #:use-module (guix packages))
 
@@ -64,3 +66,84 @@
     (description "This package provides the Google Cloud SDK which includes the
 command-line programs gsutil and gcloud among others.")
     (license license:asl2.0)))
+
+(define-public python-google-auth
+  (package
+    (name "python-google-auth")
+    (version "1.30.1")
+    (source (origin
+              (method url-fetch)
+	            (uri (pypi-uri "google-auth" version))
+	            (sha256
+	             (base32
+                "0a1jbv12izisjycvq7rzahpk3yn1j7hk9h8wqzmzh4l0wnqq2k84"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:tests? #f))
+    (home-page "https://github.com/googleapis/google-auth-library-python")
+    (synopsis "Google Authentication Library")
+    (description "This library simplifies using Google’s various
+server-to-server authentication mechanisms to access Google APIs.")
+    (license license:asl2.0)))
+
+(define-public python-google-api-core
+  (package
+    (name "python-google-api-core")
+    (version "1.28.0")
+    (source (origin
+              (method url-fetch)
+	            (uri (pypi-uri "google-api-core" version))
+	            (sha256
+	             (base32
+                "0mxrz5fscsp23jd1nidcl74i89n16761vvj53zfi53kjpl1nhr02"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:tests? #f))
+    (home-page "https://github.com/googleapis/google-cloud-python")
+    (synopsis "Google API client core library")
+    (description "This library is not meant to stand-alone. Instead it defines
+common helpers used by all Google API clients. For more information, see the
+documentation.")
+    (license license:asl2.0)))
+
+(define-public python-google-cloud-core
+  (package
+    (name "python-google-cloud-core")
+    (version "1.6.0")
+    (source (origin
+              (method url-fetch)
+	            (uri (pypi-uri "google-cloud-core" version))
+	            (sha256
+	             (base32
+                "00mvgh4vm4z5mjnlbiigmp674dwsrrsxxi7ghby7jlsl4y2v3ay6"))))
+    (build-system python-build-system)
+    (arguments
+     `(#:tests? #f))
+    (home-page "https://github.com/googleapis/google-cloud-python")
+    (synopsis "Google Cloud API client core library")
+    (description "This library is not meant to stand-alone. Instead it defines
+common helpers (e.g. base Client classes) used by all of the google-cloud-*
+packages.")
+    (license license:asl2.0)))
+
+
+(define-public python-google-resumable-media
+  (package
+    (name "python-google-resumable-media")
+    (version "1.3.0")
+    (source (origin
+              (method url-fetch)
+	            (uri (pypi-uri "google-resumable-media" version))
+	            (sha256
+	             (base32
+                "01d34c66ymriav93faf2vf89mmbcifz693vcp38sm3yidl76a2h3"))))
+    (build-system python-build-system)
+    (propagated-inputs
+     `(("python-cachetools", python-cachetools)))
+    (arguments
+     `(#:tests? #f))
+    (home-page "https://github.com/googleapis/google-resumable-media-python")
+    (synopsis "Utilities for Google Media Downloads and Resumable Uploads")
+    (description "This package provides utilities for Google media downloads
+and resumable uploads. See the docs for examples and usage.")
+    (license license:expat)))
