daad62a63d5af2dd0ab809abaf399ca6af6ef9e7
Ricardo Wurmus <rekado@elephly.net>
2024-01-24 23:34:29 +0100

    python: Add python-optree.
    * guix-science/packages/python.scm (python-optree): New variable.

--- guix-science/packages/python.scm
+++ guix-science/packages/python.scm
@@ -24,6 +24,7 @@
   #:use-module (gnu packages bioinformatics)
   #:use-module (gnu packages c)
   #:use-module (gnu packages check)
+  #:use-module (gnu packages cmake)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages elf)
@@ -67,6 +68,38 @@
   #:use-module (guix-science build-system bazel)
   #:use-module (guix-science packages machine-learning)
   #:use-module (ice-9 match))
+
+(define-public python-optree
+  (package
+    (name "python-optree")
+    (version "0.10.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "optree" version))
+       (sha256
+        (base32 "13n98dzpyavzbj1cibkimxvki0whqx0x310p361m0dlpz608hznw"))))
+    (build-system pyproject-build-system)
+    (arguments
+     (list
+      #:test-flags
+      ;; These all need a "helpers" module that is not included.
+      '(list "--ignore=tests/test_ops.py"
+             "--ignore=tests/test_typing.py"
+             "--ignore=tests/test_treespec.py"
+             "--ignore=tests/test_prefix_errors.py")))
+    (propagated-inputs (list python-typing-extensions))
+    (native-inputs
+     (list cmake-minimal
+           pybind11
+           python-pytest
+           python-pytest-cov
+           python-pytest-xdist))
+    (home-page "https://github.com/metaopt/optree")
+    (synopsis "Optimized PyTree utilities")
+    (description "This package provides optimized @code{PyTree}
+utilities.")
+    (license license:asl2.0)))
 
 (define-public python-morfessor
   (package
